859de7b605dc2e4f400ce65eb05cca44cae240a8
Dharmen <shhdharmen@gmail.com>
2018-05-11 00:44:45 +0530

    converting args

--- src/android/IonicAndroidLogs.java
+++ src/android/IonicAndroidLogs.java
@@ -1,8 +1,10 @@
 package com.dharmenshah.cordova.plugin;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
+import android.app.AlertDialog;
+import android.widget.Toast;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,21 +14,68 @@ import org.json.JSONObject;
  */
 public class IonicAndroidLogs extends CordovaPlugin {
 
-    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        switch (action) {
+        case "info":
+            String infoMessage = this.convertArgs(args.getString(0));
+            this.info(infoMessage, callbackContext);
             return true;
+        case "error":
+            String errorMessage = this.convertArgs(args.getString(0));
+            this.error(errorMessage, callbackContext);
+            return true;
+        default:
+            return false;
         }
-        return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
+    private void info(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void error(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private String convertArgs(Object args) {
+        // Toast toast = Toast.makeText(cordova.getActivity(), "Calling convertArgs",
+        // Toast.LENGTH_SHORT);
+
+        // Display toast
+        // toast.show();
+        if (args instanceof String || args instanceof Number) {
+            Toast toastStr = Toast.makeText(cordova.getActivity(), "Calling convertArgs for string or number : " + args,
+                    Toast.LENGTH_SHORT);
+            toastStr.show();
+            return String.valueOf(args);
+        } else if (args instanceof Object[]) {
+            Toast toastStr = Toast.makeText(cordova.getActivity(), "Calling convertArgs for array", Toast.LENGTH_SHORT);
+            toastStr.show();
+            Object[] argsArray = (Object[]) args;
+            String str = "";
+            for (int i = 0; i < ((Object[]) args).length; i++) {
+                str += this.convertArgs(argsArray[i]);
+            }
+            return str;
+        } else if (args instanceof JSONObject) {
+            Toast toastStr = Toast.makeText(cordova.getActivity(),
+                    "Calling convertArgs for JSONObject : " + args.toString(), Toast.LENGTH_SHORT);
+            toastStr.show();
+            JSONObject json = (JSONObject) args;
+            return json.toString();
+        } else {
+            Toast toastStr = Toast.makeText(cordova.getActivity(),
+                    "Calling convertArgs for no instance of : " + args.toString(), Toast.LENGTH_SHORT);
+            toastStr.show();
+            return args.toString();
         }
     }
 }
--- www/IonicAndroidLogs.js
+++ www/IonicAndroidLogs.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'IonicAndroidLogs', 'coolMethod', [arg0]);
+exports.info = function (arg0, success, error) {
+    exec(success, error, 'IonicAndroidLogs', 'info', [arg0]);
+};
+
+exports.error = function (arg0, success, error) {
+    exec(success, error, 'IonicAndroidLogs', 'error', [arg0]);
 };
